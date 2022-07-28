0d8d44964b57a6c2b7e212cf243b83326b621e53
Enrique Nieloud <enieloud@gmail.com>
2022-07-28 15:52:53 -0300

    Shows difficulty when the category is selected

---
+++ TriviaWithEpoxy/Extensions/UICollectionViewCompositionalLayout+List.swift
@@ -0,0 +1,97 @@
+// Created by Tyler Hedrick on 1/12/21.
+// Copyright © 2021 Airbnb Inc. All rights reserved.
+
+import Foundation
+import UIKit
+
+// MARK: - UICollectionViewCompositionalLayout
+
+extension UICollectionViewCompositionalLayout {
+  static var list: UICollectionViewCompositionalLayout {
+    if #available(iOS 14, *) {
+      return UICollectionViewCompositionalLayout { _, layoutEnvironment in
+        .list(layoutEnvironment: layoutEnvironment)
+      }
+    }
+    return listNoDividers
+  }
+
+  static var listWithHeader: UICollectionViewCompositionalLayout {
+    if #available(iOS 14, *) {
+      return UICollectionViewCompositionalLayout { _, layoutEnvironment in
+        .listWithHeader(layoutEnvironment: layoutEnvironment)
+      }
+    }
+    return listNoDividers
+  }
+
+  static var listNoDividers: UICollectionViewCompositionalLayout {
+    UICollectionViewCompositionalLayout { _, _ in
+      let item = NSCollectionLayoutItem(
+        layoutSize: .init(widthDimension: .fractionalWidth(1.0), heightDimension: .estimated(50)))
+
+      let group = NSCollectionLayoutGroup.vertical(
+        layoutSize: .init(widthDimension: .fractionalWidth(1.0), heightDimension: .estimated(50)),
+        subitems: [item])
+
+      return NSCollectionLayoutSection(group: group)
+    }
+  }
+}
+
+// MARK: - NSCollectionLayoutSection
+
+extension NSCollectionLayoutSection {
+  static var carouselWithHeader: NSCollectionLayoutSection {
+    let item = NSCollectionLayoutItem(
+      layoutSize: .init(widthDimension: .fractionalWidth(0.8), heightDimension: .estimated(50)))
+
+    let group = NSCollectionLayoutGroup.horizontal(
+      layoutSize: .init(widthDimension: .fractionalWidth(0.8), heightDimension: .estimated(50)),
+      subitems: [item])
+    group.contentInsets = .zero
+
+    let section = NSCollectionLayoutSection(group: group)
+    section.orthogonalScrollingBehavior = .groupPaging
+
+    let sectionHeader = NSCollectionLayoutBoundarySupplementaryItem(
+      layoutSize: .init(widthDimension: .fractionalWidth(1), heightDimension: .estimated(50)),
+      elementKind: UICollectionView.elementKindSectionHeader,
+      alignment: .top)
+
+    section.boundarySupplementaryItems = [sectionHeader]
+
+    return section
+  }
+
+  static func listWithHeader(
+    layoutEnvironment: NSCollectionLayoutEnvironment)
+    -> NSCollectionLayoutSection
+  {
+    let section = list(layoutEnvironment: layoutEnvironment)
+
+    let sectionHeader = NSCollectionLayoutBoundarySupplementaryItem(
+      layoutSize: .init(widthDimension: .fractionalWidth(1), heightDimension: .estimated(50)),
+      elementKind: UICollectionView.elementKindSectionHeader,
+      alignment: .top)
+
+    section.boundarySupplementaryItems = [sectionHeader]
+
+    return section
+  }
+
+  static func list(layoutEnvironment: NSCollectionLayoutEnvironment) -> NSCollectionLayoutSection {
+    if #available(iOS 14, *) {
+      return .list(using: .init(appearance: .plain), layoutEnvironment: layoutEnvironment)
+    }
+
+    let item = NSCollectionLayoutItem(
+      layoutSize: .init(widthDimension: .fractionalWidth(1.0), heightDimension: .estimated(50)))
+
+    let group = NSCollectionLayoutGroup.vertical(
+      layoutSize: .init(widthDimension: .fractionalWidth(1.0), heightDimension: .estimated(50)),
+      subitems: [item])
+
+    return NSCollectionLayoutSection(group: group)
+  }
+}
--- TriviaWithEpoxy/InitialViewController.swift
+++ TriviaWithEpoxy/InitialViewController.swift
@@ -8,34 +8,94 @@
 import UIKit
 import Epoxy
 
-class InitialViewController: CollectionViewController {
-  init() {
-    let layout = UICollectionViewCompositionalLayout
-      .list(using: .init(appearance: .plain))
-    super.init(layout: layout)
-    setItems(items, animated: false)
-  }
+let categories = ["General Knowledge", "Entertainment: Books", "Entertainment: Films"]
 
-  enum DataID {
-    case row
-  }
-
-  var count = 0 {
-    didSet {
-      setItems(items, animated: true)
+struct DifficultyLevel: Hashable {
+    static let list = ["Easy", "Medium", "Hard"]
+    var difficultyID: Int
+    var text: String {
+        get {
+            switch difficultyID {
+            case 1: return "Easy"
+            case 2: return "Medium"
+            case 3: return "Hard"
+            default: return "Easy"
+            }
+        }
     }
-  }
+}
 
-  @ItemModelBuilder
-  var items: [ItemModeling] {
-    TextRow.itemModel(
-      dataID: DataID.row,
-      content: .init(
-        title: "Count \(count)",
-        body: "Tap to increment"),
-      style: .large)
-      .didSelect { [weak self] _ in
-        self?.count += 1
-      }
-  }
+class InitialViewController: NavigationController {
+    
+    private enum DataID: Hashable {
+        case category
+        case difficulty(DifficultyLevel)
+    }
+    
+    private struct State {
+        var selectDifficulty: DifficultyLevel?
+    }
+    
+    private var state = State() {
+        didSet {
+            setStack(stack, animated: true)
+        }
+    }
+    
+    init() {
+        super.init(wrapNavigation: NavigationWrapperViewController.init(navigationController:))
+        setStack(stack, animated: false)
+    }
+    
+    @NavigationModelBuilder private var stack: [NavigationModel] {
+        NavigationModel.root(dataID: DataID.category) { [weak self] in
+            self?.createCategoriesViewController()
+        }
+        if let selectDifficulty = state.selectDifficulty {
+            NavigationModel(
+                dataID: DataID.difficulty(selectDifficulty),
+                makeViewController: { [weak self] in
+                    self?.createSelectDifficulty()
+                },
+                remove: { [weak self] in
+                    self?.state.selectDifficulty = nil
+                })
+        }
+    }
+    
+    private func createCategoriesViewController() -> UIViewController {
+        let viewController = CollectionViewController(
+            layout: UICollectionViewCompositionalLayout.list,
+            items: {
+                categories.map { category in
+                    TextRow.itemModel(
+                        dataID: category,
+                        content: .init(title: category, body: category),
+                        style: .small)
+                    .didSelect { [weak self] _ in
+                        self?.state.selectDifficulty = DifficultyLevel(difficultyID: 1)
+                    }
+                }
+            })
+        viewController.title = "Select Category"
+        return viewController
+    }
+    
+    private func createSelectDifficulty() -> UIViewController {
+        let viewController = CollectionViewController(
+            layout: UICollectionViewCompositionalLayout.list,
+            items: {
+                DifficultyLevel.list.map { d in
+                    TextRow.itemModel(
+                        dataID: d,
+                        content: .init(title: d, body: d),
+                        style: .small)
+                    .didSelect { [weak self] _ in
+                        //self?.state.showExample = example
+                    }
+                }
+            })
+        viewController.title = "Select Category"
+        return viewController
+    }
 }
