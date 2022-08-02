41c8a4322a1465bc337f5e345c9ab8a569f4ac7e
Enrique Nieloud <enieloud@gmail.com>
2022-08-02 10:30:04 -0300

    feat: Show next question when an answer was selected

--- TriviaWithEpoxy/Views/ViewControllers/QuestionViewController.swift
+++ TriviaWithEpoxy/Views/ViewControllers/QuestionViewController.swift
@@ -20,12 +20,14 @@ class QuestionViewController: CollectionViewController {
     private struct State {
         let possibleAnswers: [AnswerItem]
         let currentQuestion: String
+        let answerChecked: Bool
     }
     
     private var state: State {
         didSet {
             setItems(items, animated: true)
             topBarInstaller.setBars(topBars, animated: true)
+            bottomBarInstaller.setBars(bottomBars, animated: true)
         }
     }
     
@@ -33,9 +35,9 @@ class QuestionViewController: CollectionViewController {
         let layout = UICollectionViewCompositionalLayout
             .list(using: .init(appearance: .plain))
         self.game = game
-        self.state = State(possibleAnswers: [], currentQuestion: "")
+        self.state = State(possibleAnswers: [], currentQuestion: "", answerChecked: false)
         super.init(layout: layout)
-        createStateFromGame()
+        createStateFromGame(answerChecked: false)
         setItems(items, animated: false)
     }
     
@@ -50,22 +52,31 @@ class QuestionViewController: CollectionViewController {
     
     @ItemModelBuilder
     var items: [ItemModeling] {
-        state.possibleAnswers.map { answerItem in
-            TextRow.itemModel(
-                dataID: answerItem.answerID,
-                content: .init(title: answerItem.text, body: nil),
-                style: answerItem.selected ? TextRow.Style.large : TextRow.Style.small)
-            .didSelect { [weak self] context in
-                self?.selectItem(id: answerItem.answerID)
+        if !state.answerChecked {
+            state.possibleAnswers.map { answerItem in
+                TextRow.itemModel(
+                    dataID: answerItem.answerID,
+                    content: .init(title: answerItem.text, body: nil),
+                    style: answerItem.selected ? TextRow.Style.large : TextRow.Style.small)
+                .didSelect { [weak self] context in
+                    self?.selectItem(id: answerItem.answerID)
+                }
+            }
+        } else {
+            state.possibleAnswers.enumerated().map { (index,answerItem) in
+                TextRow.itemModel(
+                    dataID: answerItem.answerID,
+                    content: .init(title: answerItem.text, body: nil),
+                    style: game.isCorrect(index: index) ? TextRow.Style.large : TextRow.Style.small)
             }
         }
     }
     
-    private func createStateFromGame() {
+    private func createStateFromGame(answerChecked: Bool) {
         let possibleAnswers = game.possibleAnswers.enumerated().map { (index, possibleAnswer) in
             AnswerItem(selected: false, text: possibleAnswer, answerID: getItemID(position: index))
         }
-        state = State(possibleAnswers: possibleAnswers, currentQuestion: game.questionStr)
+        state = State(possibleAnswers: possibleAnswers, currentQuestion: game.questionStr, answerChecked: answerChecked)
     }
     
     private func getItemID(position: Int) -> Int {
@@ -79,7 +90,7 @@ class QuestionViewController: CollectionViewController {
                     selected: idx == indexFound,
                     text: item.text,
                     answerID: item.answerID) }
-            self.state = State(possibleAnswers: possibleAnswers, currentQuestion: state.currentQuestion)
+            self.state = State(possibleAnswers: possibleAnswers, currentQuestion: state.currentQuestion, answerChecked: false)
         }
     }
     
@@ -99,24 +110,28 @@ class QuestionViewController: CollectionViewController {
     @BarModelBuilder
     var bottomBars: [BarModeling] {
         ButtonRow.barModel(
-            content: .init(text: "Check answer!"),
+            content: .init(text: self.state.answerChecked ? "Next Question" : "Check answer!"),
             behaviors: .init(didTap: {
-                if let indexFound = self.indexOfSelection() {
-                    if self.game.evalAnswer(index: indexFound) {
-                        self.showText(title: "Your answer is:", message: "correct answer")
-                    } else {
-                        self.showText(title: "Your answer is:", message: "incorrect answer")
-                    }
+                if self.state.answerChecked {
                     self.game.next()
-                    self.createStateFromGame()
+                    self.createStateFromGame(answerChecked: false)
+                } else {
+                    if let indexFound = self.indexOfSelection() {
+                        if self.game.evalAnswer(index: indexFound) {
+                            self.showText(title: "Correct answer!", message: "")
+                        } else {
+                            self.showText(title: "Oops...", message: "incorrect answer")
+                        }
+                        self.createStateFromGame(answerChecked: true)
+                    }
                 }
             }))
     }
     
     @BarModelBuilder
     var topBars: [BarModeling] {
-        TextRow.barModel(content: TextRow.Content(title: "Game Type", body: game.description()), style: TextRow.Style.large)
-        TextRow.barModel(content: TextRow.Content(title: state.currentQuestion, body: "Choose your answer"), style: TextRow.Style.large)
+        TextRow.barModel(content: TextRow.Content(title: game.description(), body: nil), style: TextRow.Style.small)
+        TextRow.barModel(content: TextRow.Content(title: nil, body: state.currentQuestion), style: TextRow.Style.large)
     }
     
     func showText(title: String, message: String) {
